316284bde53a427d751df167d789c1e7dd1c69ef
tushxr12 <tusharforcourses@gmail.com>
2024-07-27 17:29:31 +0530

    Add: About component

---
+++ Frontend/src/about-us/AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import About from "../components/About";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+function AboutUs() {
+  return (
+    <div>
+      <Navbar />
+      <About />
+      <Footer />
+    </div>
+  );
+}
+
+export default AboutUs;
---
+++ Frontend/src/components/About.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function About() {
+  return (
+    <>
+      <div className="mt-20 max-w-4xl mx-auto p-6 sm:p-8 lg:p-12 shadow-md rounded-lg">
+        <h2 className="text-3xl font-bold text-white-800 mb-4 sm:mb-6">
+          About Us
+        </h2>
+        <p className="text-white-700 leading-relaxed">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non
+          leo nec felis pharetra ultrices a eu tortor. Sed interdum mi in nisi
+          facilisis, vel egestas nisi posuere. Integer at purus euismod, posuere
+          mi a, posuere mauris. Aenean eu dui at nunc egestas semper. Vestibulum
+          ut turpis a urna tristique vestibulum et et libero. Nulla facilisi. In
+          hac habitasse platea dictumst. Fusce tempor orci non magna tincidunt,
+          eu convallis urna ornare. Donec euismod, est in malesuada fermentum,
+          purus justo feugiat eros, eget fermentum elit lectus in risus.
+          Pellentesque habitant morbi tristique senectus et netus et malesuada
+          fames ac turpis egestas. Suspendisse potenti. Donec varius eros et
+          velit convallis, eget scelerisque arcu efficitur. Lorem ipsum dolor
+          sit amet, consectetur adipiscing elit. Curabitur non leo nec felis
+          pharetra ultrices a eu tortor. Sed interdum mi in nisi facilisis, vel
+          egestas nisi posuere. Integer at purus euismod, posuere mi a, posuere
+          mauris. Aenean eu dui at nunc egestas semper. Vestibulum ut turpis a
+          urna tristique vestibulum et et libero. Nulla facilisi. In hac
+          habitasse platea dictumst. Fusce tempor orci non magna tincidunt, eu
+          convallis urna ornare. Donec euismod, est in malesuada fermentum,
+          purus justo feugiat eros, eget fermentum elit lectus in risus.
+          Pellentesque habitant morbi tristique senectus et netus et malesuada
+          fames ac turpis egestas. Suspendisse potenti. Donec varius eros et
+          velit convallis, eget scelerisque arcu efficitur.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default About;
