7d6c773b414eb920b1a3496c9c41cb26cecb90e3
tushxr12 <tusharforcourses@gmail.com>
2024-08-04 14:50:23 +0530

    Make course route protected

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,31 @@
 import React from "react";
 import Home from "./home/Home.jsx";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Courses from "./courses/Courses.jsx";
 import Signup from "./components/Signup.jsx";
 import Contact from "./components/Contact.jsx";
 import ContactUs from "./contact-us/ContactUs.jsx";
 import AboutUs from "./about-us/AboutUs.jsx";
+import { Toaster } from "react-hot-toast";
+import { useAuth } from "./context/AuthProvider.jsx";
 
 function App() {
+  const [authUser, setAuthUser] = useAuth();
+  console.log(authUser);
   return (
     <>
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/course" element={<Courses />} />
+          <Route
+            path="/course"
+            element={authUser ? <Courses /> : <Navigate to="/signup" />}
+          />
           <Route path="/signup" element={<Signup />} />
           <Route path="/contact" element={<ContactUs />} />
           <Route path="/about" element={<AboutUs />} />
         </Routes>
+        <Toaster />
       </div>
     </>
   );
