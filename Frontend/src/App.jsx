14f5c465b3f592b5415d5d1d2930d04b2db28814
tushxr12 <tusharforcourses@gmail.com>
2024-08-09 20:08:35 +0530

    Remove unused import statements

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,6 @@ import Home from "./home/Home.jsx";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Courses from "./courses/Courses.jsx";
 import Signup from "./components/Signup.jsx";
-import Contact from "./components/Contact.jsx";
 import ContactUs from "./contact-us/ContactUs.jsx";
 import AboutUs from "./about-us/AboutUs.jsx";
 import { Toaster } from "react-hot-toast";
